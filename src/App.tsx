45794a0af9aba877f5d678aa2bb76d05e9be03e3
ishmeet-codes <ishmeetsinghxa@gmail.com>
2025-04-28 22:56:59 +0530

    JMK_38: Dependencies Updation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.tsx
+import { BrowserRouter as Router } from "react-router-dom";
+import { useEffect } from "react";
+import { AuthProvider } from "../src/Pages/Client/AuthContext"; 
+import { SignupProvider } from "./Pages/Freelancer/SignupContext";
+import AppRoutes from "./router/AppRoutes";
+import './index.css';
+import { initMockUser } from "./utils/initUser";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    initMockUser();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <SignupProvider>
+      <AuthProvider>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </AuthProvider>
+    </SignupProvider>
+  );
 }
 
-export default App
+export default App;
