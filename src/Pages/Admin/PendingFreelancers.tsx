700047b8fdc93f4a64e32288836a3cd7a3b9b364
Dhruvjot-codes <dhruvjotsingh21@gmail.com>
2025-09-04 15:17:32 +0530

    JMK_145: cut console  review code

--- src/Pages/Admin/PendingFreelancers.tsx
+++ src/Pages/Admin/PendingFreelancers.tsx
@@ -25,7 +25,7 @@ export default function PendingFreelancers() {
     const fetchData = async () => {
       try {
         const data = await getPendingFreelancers();
-        console.log("✅ API response:", data);
+       
 
         if (Array.isArray(data) && data.length > 0) {
           setFreelancers(data);
